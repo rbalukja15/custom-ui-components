85495ee54ec5643da7a7792a034f906454364ece
Romarjo Balukja <r.balukja@foodtracks.de>
2023-05-05 10:39:16 +0000

    v0.5.0

--- src/components/atoms/Slider/Slider.stories.tsx
+++ src/components/atoms/Slider/Slider.stories.tsx
@@ -1,29 +1,52 @@
-import { Story } from '@storybook/react'
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
 
-import { RangeSlider, RangeSliderProps } from './index'
+import { RangeSlider } from './index'
+import { BASE_COLORS } from '../../../themes/BaseColors'
 
-const Template: Story<RangeSliderProps> = args => <RangeSlider {...args} />
+const SliderExample = props => {
+  const [amountValue, setAmountValue] = React.useState<number | number[]>(props.value)
+  const sx = {
+    width: '80%',
+    p: 5,
+  }
 
-export const Default = Template.bind({})
-Default.args = {
-  percentage: 30,
-  barColor: '#85c6ff',
-  fillerColor: '#066fcd',
+  return (
+    <RangeSlider
+      {...props}
+      sx={sx}
+      value={amountValue}
+      onRangeChange={(event, newValue) => setAmountValue(newValue as number[])}
+    />
+  )
 }
 
-export default {
-  title: 'Atoms/RangeSlider',
-  component: RangeSlider,
-  argTypes: {
-    barColor: {
-      control: {
-        type: 'color',
-      },
-    },
-    fillerColor: {
-      control: {
-        type: 'color',
-      },
-    },
-  },
-}
+storiesOf('atoms/RangeSlider', module)
+  .add('Slider with 2 limits', () => (
+    <SliderExample
+      min={-50}
+      max={100}
+      value={[20, 37]}
+      barColor={BASE_COLORS.COLOR_RED_800}
+      fillerColor={BASE_COLORS.COLOR_GREEN_500}
+    />
+  ))
+  .add('Slider with 1 limit', () => (
+    <SliderExample
+      min={-50}
+      max={100}
+      value={20}
+      barColor={BASE_COLORS.COLOR_RED_800}
+      fillerColor={BASE_COLORS.COLOR_GREEN_500}
+    />
+  ))
+  .add('Slider inverted track', () => (
+    <SliderExample
+      min={-50}
+      max={100}
+      value={20}
+      barColor={BASE_COLORS.COLOR_RED_800}
+      fillerColor={BASE_COLORS.COLOR_GREEN_500}
+      track="inverted"
+    />
+  ))
--- src/components/atoms/Slider/index.tsx
+++ src/components/atoms/Slider/index.tsx
@@ -1,39 +1,82 @@
+import { SxProps } from '@mui/material'
 import Box from '@mui/material/Box'
 import Slider from '@mui/material/Slider'
-import { styled, useTheme } from '@mui/material/styles'
+import { styled, Theme, useTheme } from '@mui/material/styles'
 import * as React from 'react'
 
-function valuetext(value: number) {
-  return `${value}°C`
-}
+import { BASE_COLORS } from '../../../themes/BaseColors'
 
-export interface PrettoSliderProps {
-  fillerColor?: string
-  barColor?: string
+function valuetext(value: number) {
+  return `${value}`
 }
 
 export interface RangeSliderProps {
+  min: number
+  max: number
+  value: number | number[]
   fillerColor?: string
   barColor?: string
+  track?: 'normal' | false | 'inverted'
+  sx?: SxProps<Theme> | undefined
+  onRangeChange?: (event: Event, value: number | number[]) => void
 }
 
-const PrettoSlider = styled(Slider)(({ fillerColor, barColor }: PrettoSliderProps) => ({
-  color: barColor,
+export interface StyledSliderProps extends RangeSliderProps {
+  lowerValue: number
+}
+
+const getColor = (props: StyledSliderProps) => {
+  if (props.lowerValue < 0) {
+    return `linear-gradient(to right, ${BASE_COLORS.COLOR_PURPLE_600} 0%, ${props.barColor} ${calculateMarkPercentage(
+      props.min,
+      props.max
+    )}%, ${props.barColor} 100%)`
+  }
+  if (props.track === 'inverted') {
+    return BASE_COLORS.COLOR_GREEN_500
+  }
+  return `linear-gradient(to right, ${props.barColor} 0%, ${props.barColor} 100%)`
+}
+
+const getTrack = (props: StyledSliderProps) => {
+  if (props.track === 'inverted') {
+    return `linear-gradient(to left, ${BASE_COLORS.COLOR_PINK_300} 0%, ${
+      BASE_COLORS.COLOR_PINK_300
+    } ${calculateMarkPercentage(props.min, props.max)}%, ${props.barColor} 100%)`
+  }
+  return BASE_COLORS.COLOR_GREEN_500
+}
+
+const calculateMarkPercentage = (min: number, max: number) => {
+  const upper = Math.abs(min)
+  const lower = Math.abs(min) + max
+  return (upper / lower) * 100
+}
+
+export const StyledSlider = styled(Slider, {
+  shouldForwardProp: prop =>
+    prop !== 'lowerValue' && prop !== 'fillerColor' && prop !== 'barColor' && prop !== 'onRangeChange' && prop !== 'sx',
+})<StyledSliderProps>(props => ({
   height: 8,
   '& .MuiSlider-track': {
     border: 'none',
+    background: getTrack(props),
   },
   '& .MuiSlider-thumb': {
     height: 24,
     width: 24,
-    backgroundColor: fillerColor,
-    border: '2px solid currentColor',
+    border: `1px solid ${props.fillerColor}`,
     '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
       boxShadow: 'inherit',
     },
     '&:before': {
       display: 'none',
     },
+  },
+  '& .MuiSlider-rail': {
+    background: getColor(props),
+    transition: 'background-color 0.2s',
+    transitionTimingFunction: 'ease-in-out',
   },
   '& .MuiSlider-valueLabel': {
     lineHeight: 1.2,
@@ -43,7 +86,7 @@ const PrettoSlider = styled(Slider)(({ fillerColor, barColor }: PrettoSliderProp
     width: 32,
     height: 32,
     borderRadius: '50% 50% 50% 0',
-    backgroundColor: fillerColor,
+    backgroundColor: props.fillerColor,
     transformOrigin: 'bottom left',
     transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
     '&:before': { display: 'none' },
@@ -58,17 +101,37 @@ const PrettoSlider = styled(Slider)(({ fillerColor, barColor }: PrettoSliderProp
 
 export const RangeSliderComponent: React.FC<RangeSliderProps> = ({ ...props }) => {
   const theme = useTheme()
-  const { fillerColor = theme.palette.primary.dark, barColor = theme.palette.primary.dark, ...rest } = props
+  const {
+    fillerColor = theme.palette.primary.main,
+    barColor = theme.palette.primary.main,
+    min,
+    max,
+    value,
+    sx,
+    onRangeChange,
+    ...rest
+  } = props
 
-  const [value, setValue] = React.useState<number[]>([20, 37])
+  const marks = [
+    {
+      value: min,
+      label: `${min}`,
+    },
+    {
+      value: 0,
+      label: '0',
+    },
+    { value: max, label: `${max}` },
+  ]
 
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number[])
+  const handleChange = (event: Event, newValue: number[]) => {
+    onRangeChange && onRangeChange(event, newValue)
   }
 
   return (
-    <Box sx={{ width: 300 }}>
-      <PrettoSlider
+    <Box sx={sx}>
+      <StyledSlider
+        data-testid={'custom-range-slider'}
         getAriaLabel={() => 'Temperature range'}
         value={value}
         onChange={handleChange}
@@ -76,6 +139,10 @@ export const RangeSliderComponent: React.FC<RangeSliderProps> = ({ ...props }) =
         getAriaValueText={valuetext}
         barColor={barColor}
         fillerColor={fillerColor}
+        marks={marks}
+        lowerValue={value[0]}
+        min={min}
+        max={max}
         {...rest}
       />
     </Box>
